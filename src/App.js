560eff624406d47d356c7b67ee56f084fab354e2
juandavc12 <67342738+juandavc12@users.noreply.github.com>
2020-10-22 19:58:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+  
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
+
+	constructor(props){
+		super(props);
+		this.state = {
+			count: 0,
+		} 
+    
+    	updateCount(event){
+		this.setState({
+			count: event.target.value.length
+	    });
+	}
+	}
+
+	render() {
+		return (
+			<div className="container">
+			<textarea className={this.state.count > 79 ? "textarea-error" : ""} rows="3" onChange={this.updateCount.bind(this)}></textarea>
+			<div className={this.state.count > 79 ? "counter-error" : "counter"}>{this.state.count}</div>
+			</div>
+			);
+	}
+
+
 }
 
 export default App;
